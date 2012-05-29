7bdfea1b4454db239407653238f7b52f1b27cf3a
Tim Benniks <tbenniks@gmail.com>
2012-05-29 22:29:43 +0200

    Added feed and others fancy shizl

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -18,7 +18,7 @@ module Jekyll
     def convert(content)
       begin
         puts "Performing Sass Conversion."
-        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./css/"], :style => :compressed)
+        engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./static/css/"], :style => :compressed)
         engine.render
       rescue StandardError => e
         puts "!!! SASS Error: " + e.message
