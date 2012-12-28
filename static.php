1bb1d2354e3585ac1e5cffcff3c1397f638787f4
Tim Benniks <tbenniks@gmail.com>
2012-12-28 22:19:07 +0100

    - seo updates, speed improvements

--- static.php
+++ static.php
@@ -36,9 +36,36 @@ function make_gzip($source)
 	}
 }
 
+function html_compress($html)
+{
+    preg_match_all('!(<(?:code|pre|script).*>[^<]+</(?:code|pre|script)>)!',$html,$pre);
+    $html = preg_replace('!<(?:code|pre).*>[^<]+</(?:code|pre)>!', '#pre#', $html);
+    $html = preg_replace('#<!–[^\[].+–>#', '', $html);
+    $html = preg_replace('/[\r\n\t]+/', ' ', $html);
+    $html = preg_replace('/>[\s]+</', '><', $html);
+    $html = preg_replace('/[\s]+/', ' ', $html);
+    
+    if(!empty($pre[0]))
+    {
+        foreach ($pre[0] as $tag)
+        {
+            $html = preg_replace('!#pre#!', $tag, $html,1);
+        }
+    }
+    
+    return $html;
+}
+
 if(file_exists($file.'.'.$ext))
 {
-    echo make_gzip(file_get_contents($file.'.'.$ext));
+    //if($ext == 'html')
+    //{
+	//    echo make_gzip(html_compress(file_get_contents($file.'.'.$ext)));
+    //}
+    //else
+    //{
+    	echo make_gzip(file_get_contents($file.'.'.$ext));
+    //}
 }
 
 ?>
